cec5fa4d94e418212bba340859b56d1fd6386e33
riidefi <34194588+riidefi@users.noreply.github.com>
2020-11-25 22:05:01 -0500

    :white_check_mark: Filesystem tests

--- source/test.cxx
+++ source/test.cxx
@@ -1,3 +1,5 @@
+#define KURIBO_ENABLE_LOG
+
 #include "modules/gecko/Frontend.hxx"
 
 #include <EASTL/array.h>
@@ -14,53 +16,85 @@
 #include "system/system.hxx"
 #include <core/patch.hxx>
 
+#include <io/filesystem.hxx>
+#include <io/io.hxx>
+
 void GeckoJIT_RunTests();
 
 namespace tests {
-    void CodeJIT() {
-KURIBO_SCOPED_LOG("Code Parsing Test");
+void CodeJIT() {
+  {
+    KURIBO_SCOPED_LOG("Code Parsing Test");
 
-      
-      GeckoJIT_RunTests();
+    //GeckoJIT_RunTests();
+  }
 
-    }
-    void CodeParser() {
-        KURIBO_SCOPED_LOG("Code Parsing Test");
+  eastl::string_view search_path = "Race/Course/castle_course.szs";
+  kuribo::io::fs::InitFilesystem();
+  u32 rvl_e, kur_e;
+#ifndef _WIN32
+  {
+    KURIBO_SCOPED_LOG("Revolution Search");
 
-        // Create a read-only view.
-        const eastl::string_view code_example = "$Proc Mon PAL\n042153B8 4E800020\n$OtherCode\n04238F14 4E800020\n04009600 48239100";
+    rvl_e = kuribo::io::dvd::queryEntryNum(search_path.data());
+  }
+  KURIBO_LOG("REVOLUTION: %u\n", rvl_e);
+#endif
+  {
+    KURIBO_SCOPED_LOG("Kuribo Search");
 
-        // Construct a lexical parser.
-        kuribo::gecko::CodeParser<kuribo::gecko::CodePrinter> parser{ code_example };
+    kur_e = kuribo::io::fs::Path(search_path).getResolved();
+  }
+  KURIBO_LOG("KURIBO: %u\n", kur_e);
+  {
+    kuribo::io::fs::Path p("Race");
+    p /= "Course";
+    p /= "Beginner_course.szs";
+    kur_e = p.getResolved();
+  }
+  KURIBO_LOG("KURIBO: %u\n", kur_e);
 
-        // Attach a temporary CodePrinter to receive parsing actions.
-        kuribo::gecko::CodePrinter printer;
+  for (auto it : kuribo::io::fs::RecursiveDirectoryIterator("Pokey")) {
+    KURIBO_LOG("How's this? %s\n", it.getName());
+  }
+}
+void CodeParser() {
+  KURIBO_SCOPED_LOG("Code Parsing Test");
 
-        parser.parse(printer);
-    }
+  // Create a read-only view.
+  const eastl::string_view code_example = "$Proc Mon PAL\n042153B8 4E800020\n$OtherCode\n04238F14 4E800020\n04009600 48239100";
+
+  // Construct a lexical parser.
+  kuribo::gecko::CodeParser<kuribo::gecko::CodePrinter> parser{ code_example };
+
+  // Attach a temporary CodePrinter to receive parsing actions.
+  kuribo::gecko::CodePrinter printer;
+
+  parser.parse(printer);
+}
 } // namespace tests
 
 eastl::array<char, 1024 * 4 * 4 * 4> heap;
-void comet_app_install(void* image, void* vaddr_load, uint32_t load_size) {    
-    KURIBO_SCOPED_LOG("Installing...");
+void comet_app_install(void* image, void* vaddr_load, uint32_t load_size) {
+  KURIBO_SCOPED_LOG("Installing...");
 
-    heap = {};
+  heap = {};
 #ifndef _WIN32
-    for (int i = 0; i < heap.size(); i += 32)
-      kuribo::flushAddr(&heap[0] + i);
+  for (int i = 0; i < heap.size(); i += 32)
+    kuribo::flushAddr(&heap[0] + i);
 #endif
-    heap = {};
-    
+  heap = {};
 
-    const auto heap_halfsize = heap.size() / 2;
-    kuribo::mem::Init(heap.data(), heap_halfsize, heap.data() + heap_halfsize, heap_halfsize);
-    tests::CodeJIT();
-    tests::CodeParser();
-    if (kuribo::BuildPlatform != kuribo::platform::PC) {
-        // tests::CodeParserFromDisc();
-    }
 
-    kuribo::System::createSystem();
+  const auto heap_halfsize = heap.size() / 2;
+  kuribo::mem::Init(heap.data(), heap_halfsize, heap.data() + heap_halfsize, heap_halfsize);
+  tests::CodeJIT();
+  tests::CodeParser();
+  if (kuribo::BuildPlatform != kuribo::platform::PC) {
+    // tests::CodeParserFromDisc();
+  }
+
+  kuribo::System::createSystem();
 }
 
 /* Defines the function addresses in PAL MKW */
--- source/types.h
+++ source/types.h
@@ -8,6 +8,7 @@ typedef int16_t 	s16;
 typedef uint16_t 	u16;
 typedef int32_t		s32;
 typedef uint32_t 	u32;
+typedef uint64_t  u64;
 
 typedef float 		f32;
 typedef double 		f64;
