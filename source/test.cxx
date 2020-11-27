948f3cef7b729ea471267a7766c4157cec96b3d8
riidefi <34194588+riidefi@users.noreply.github.com>
2020-11-27 10:01:59 -0500

    :recycle: Moved host symbols into platform.cxx

--- source/test.cxx
+++ source/test.cxx
@@ -96,17 +96,3 @@ void comet_app_install(void* image, void* vaddr_load, uint32_t load_size) {
 
   kuribo::System::createSystem();
 }
-
-/* Defines the function addresses in PAL MKW */
-#define OSReport 0x801A25D0
-#define OSFatal 0x801A4EC4
-#define DVDConvertPathToEntryNum 0x8015DF4C
-#define DVDFastOpen 0x8015E254
-#define DVDReadPrio 0x8015E834
-#define DVDClose 0x8015E568
-
-/* Sets GC/Wii OS functions. */
-KURIBO_SET_OS(OSReport, OSFatal)
-
-/* Sets functions to read from the disc. */
-KURIBO_SET_DVD(DVDConvertPathToEntryNum, DVDFastOpen, DVDReadPrio, DVDClose)
